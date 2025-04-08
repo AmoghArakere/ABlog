e3b0c68c6dd53cd5d1fb67a42e5c86eddd03f949
AmoghArakere <amogh.nagaraj03@gmail.com>
2025-04-08 13:07:34 +0530

    Add Netlify Edge Functions configuration

---
+++ netlify/edge-functions/entry.js
@@ -0,0 +1,5 @@
+export default async (request, context) => {
+  // This is a simple pass-through function
+  // It will be replaced by Astro's generated edge function during build
+  return context.next();
+};
