cf94be82822dd3246c26c09256becfb92e167a7d
AmoghArakere <amogh.nagaraj03@gmail.com>
2025-04-10 01:30:03 +0530

    Add @tailwindcss/typography plugin and placeholder images

--- tailwind.config.js
+++ tailwind.config.js
@@ -105,6 +105,6 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
   darkMode: 'class',
 }
